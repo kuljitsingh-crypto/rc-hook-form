99c1d3c069a0b83d6f25ad96e5c70c1394142b28
kuljitsingh <kuljitsingh@kuljitsingh-ka-MacBook-Air.local>
2025-03-06 22:50:55 +0530

    updated types for form

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,4 @@ export type UseFormControl = ReturnType<typeof useFormHook>["control"];
 export type UseFormController = Parameters<typeof FormController>[0];
 export type UseFormValidationField = ValFields;
 export type UseFormState = ReturnType<typeof useFormHook>["formState"];
-export type UseElement = Element;
+export type UseFormElement = Element;
